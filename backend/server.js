6846f5ff5a15422319d01e5c479a3dcac4118bff
Anushka <anushkameena0612@gmail.com>
2026-05-09 10:43:46 +0530

    fixing errors

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,44 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import taskRoutes from './routes/taskRoutes.js';
-import habitRoutes from './routes/habitRoutes.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import taskRoutes from "./routes/taskRoutes.js";
+import habitRoutes from "./routes/habitRoutes.js";
+import dotenv from "dotenv";
 
 dotenv.config();
-
 
 const app = express();
 
 app.use(
-    cors({
-        origin: "https://taskmosis.vercel.app",
-        credentials: true,
-    }
+  cors({
+    origin: "https://taskmosis.vercel.app",
+    credentials: true,
+  })
+);
 
-));
 app.use(express.json());
 
-
-mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to MongoDB"));
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
-
-.catch((err) => console.log(err));
-
-app.get("/", (req, res) => {
-    res.send("API is working");
-});
-
+// routes
 app.use("/api", taskRoutes);
-
 app.use("/api/habits", habitRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
 
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
 
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
