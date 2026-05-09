d7c7db1554363d7b08bc33a61e7f4113de0f19af
Anushka <anushkameena0612@gmail.com>
2026-05-09 12:22:03 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 
-console.log("MONGO URI:", process.env.MONGO_URI);
+console.log("ENV CHECK:", process.env.MONGO_URI);
 
 mongoose
   .connect(process.env.MONGO_URI)
