771c03555cd0ab25b9ceddf29b28ee05adc8b8ff
Anushka <anushkameena0612@gmail.com>
2026-05-09 09:47:07 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://taskmosis.vercel.app/",
+        credentials: true,
+    }
+
+));
 app.use(express.json());
 
 
